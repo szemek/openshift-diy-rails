dfa7459234208f70096bdc9aed0c5966011cd885
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-05-23 23:34:19 +0200

    fix for default resolv configuration
    https://forums.openshift.com/failed-to-installed-new-gem-with-self-compiled-ruby

--- install.sh
+++ install.sh
@@ -12,7 +12,11 @@ git clone https://github.com/sstephenson/ruby-build.git $RBENV_ROOT/plugins/ruby
 
 # install ruby
 rbenv install 2.2.2
+
+# Fix https://forums.openshift.com/failed-to-installed-new-gem-with-self-compiled-ruby
+sed --in-place -r -e 's|.0.0.0.0.|ENV["OPENSHIFT_DIY_IP"]|' `find . -name resolv.rb`
+
 rbenv shell 2.2.2
 
 # install rails
-gem install rails --no-ri --no-rdoc --verbose
+gem install rails --no-ri --no-rdoc

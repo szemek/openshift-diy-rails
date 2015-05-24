034cc7c5aa90a83224aba97699c6971e9a4c530a
Przemysław Dąbek <przemyslaw.dabek@lunarlogic.io>
2015-05-24 08:53:41 +0200

    Rails 4.2.1

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Project::Application.config.secret_token = 'b6263d0e4206113f0bc509938c9c5d4ee0a3960b7738d9b5e485ae4ec6e043c5597e1e4ce3ae8c82f0cd74c069532cdac8d67297fbbbc072d720d675147d0f53'
